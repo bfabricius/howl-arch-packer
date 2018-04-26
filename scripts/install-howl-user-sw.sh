2e0439cbd309ff332010a43dd5e569545cdfc848
Ben Fabricius <ben@htaudio.de>
2018-04-26 16:59:12 +0200

    provision how-arch base with python2, python2-pip and install python requests. Fix issues #1 and #3.

--- scripts/install-howl-user-sw.sh
+++ scripts/install-howl-user-sw.sh
@@ -12,8 +12,8 @@ set -eu
 OWL_PROGRAM_GIT=https://github.com/pingdynasty/OwlProgram.git
 
 ## TEMPORARY CODE: TO BE REMOVED
-if [[ -d ${HOME}/source ]]; then
-	rm -rf ${HOME}/source
+if [[ -d /home/vagrant/source ]]; then
+	rm -rf /home/vagrant/source
 fi
 
 # extend path
@@ -22,13 +22,16 @@ echo "source ${EXPORTS_SH}" >> ${HOME}/.bashrc
 echo "alias ll='ls -alh --color=auto'" >> ${HOME}/.bashrc
 
 echo "==> Installing OWL Program Source"
-/usr/bin/mkdir ${HOME}/source
-cd ${HOME}/source
+/usr/bin/mkdir /home/vagrant/source
+cd /home/vagrant/source
 /usr/bin/git clone ${OWL_PROGRAM_GIT}
 cd OwlProgram
 
 echo "==> Building OWL patch binary"
-TOOLROOT='${TLC_TOOLROOT}/' /usr/bin/make patch
+TOOLROOT='${TLC_TOOLROOT}/' /usr/bin/make patch && make clean
+
+echo "==> Changing ownershipt of source folder"
+chown -R vagrant:vagrant /home/vagrant/source
 
 echo "==> Howl User SW installation done"
 
